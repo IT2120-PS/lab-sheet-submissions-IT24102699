b1815f5ad7882b24693237d6f8f1f32f5dfae504
IT24102699 <it24102699@my.sliit.lk>
2025-09-16 11:03:05 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102699/IT24102699.R
@@ -0,0 +1,38 @@
+
+                                      # IT24102699
+
+                                  # Mummullage B.U.T
+
+                    # IT2120 - Probability and Statistics - Lab 07
+
+
+getwd()
+setwd("F:\\SLIIT\\_Year_02_\\Semester 01\\PS - Probability and Statistics\\Lab Practicals\\Lab 07\\IT24102699")
+getwd()
+
+
+
+# Question 01
+
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+
+
+# Question 02
+
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+
+
+# Question 03
+
+## Part 01
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+## Part 02
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
+
+
+
+
+
